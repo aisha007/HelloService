c948b313488458eed407931481f27c7eee3e8b21
aisha007 <aishagogate007@gmail.com>
2021-09-23 04:12:53 -0700

    First Commit

---
+++ GitHelloService/src/main/java/MicroServices/GitHelloService/GitHelloServiceApplication.java
@@ -0,0 +1,13 @@
+package MicroServices.GitHelloService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHelloServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHelloServiceApplication.class, args);
+	}
+
+}
---
+++ GitHelloService/src/test/java/MicroServices/GitHelloService/GitHelloServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package MicroServices.GitHelloService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHelloServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
